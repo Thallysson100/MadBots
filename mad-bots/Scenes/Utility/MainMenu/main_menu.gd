f88b149bea6f064d523b8b7f7a588431009b5021
Thiago Martins <thiago.martins1112@gmail.com>
2025-11-09 18:29:10 -0300

    Adicionado menu inicial funcional e interativo, animação de transição entre cenas + atribuição do menu como cena inicial.

---
+++ mad-bots/Scenes/Utility/MainMenu/main_menu.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+func _on_start_pressed() -> void:
+	TransitionScreen.transition()
+	await TransitionScreen.on_transition_finished
+	get_tree().change_scene_to_file("res://World/world.tscn")
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()

3187ae9a509d20f6a7db21a552c0cf7d8999d95f
Thiago Martins <thiago.martins1112@gmail.com>
2025-11-11 23:08:46 -0300

    Adicionado menu de pause.

---
+++ mad-bots/Scenes/Utility/PauseMenu/pause_menu.gd
@@ -0,0 +1,35 @@
+extends Control
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _ready() -> void:
+	animation_player.play("RESET")
+
+func resume():
+	get_tree().paused = false
+	animation_player.play_backwards("blur")
+
+func pause():
+	get_tree().paused = true
+	animation_player.play("blur")
+
+func testEsc():
+	if Input.is_action_just_pressed("esc") and get_tree().paused == false:
+		pause()
+	elif Input.is_action_just_pressed("esc") and get_tree().paused == true:
+		resume()
+
+func _on_resume_pressed() -> void:
+	resume()
+
+func _on_restart_pressed() -> void:
+	resume()
+	get_tree().reload_current_scene()
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _process(delta):
+	testEsc()
